90bccf1b42977dbddf97a9b74a5c19ce54fa9d36
MaximeLeherle <69516306+MaximeLeherle@users.noreply.github.com>
2023-03-25 15:20:20 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+# Titre de l'application
+st.title("Mon application Streamlit")
+
+# Sous-titre
+st.subheader("Introduction")
+
+# Paragraphe de texte
+st.write("Bienvenue dans mon application Streamlite !")
