93040a179281951385c1a48808f618dc329f7e2d
MaximeLeherle <69516306+MaximeLeherle@users.noreply.github.com>
2023-03-25 22:20:41 +0100

    Update Dashboard.py
    Correct the issue that no winner win is the score of Maxildan is "NaN"

--- Dashboard.py
+++ Dashboard.py
@@ -1,23 +1,24 @@
-#Import
-import pandas as pd
-import numpy as np
 import streamlit as st
+import pandas as pd
 
-# Charger le fichier CSV dans un dataframe Pandas
-df = pd.read_csv("datav3.csv", delimiter=";")
+# Lecture du fichier CSV
+stat_df = pd.read_csv("datav3.csv", sep=";")
 
-# Création d'une fonction qui récupère le nom du joueur ayant le score maximal
-def get_winner(row):
-    scores = row[2:]
+# Création d'une fonction qui récupère le nom ou les noms des joueurs ayant le score maximal
+def get_winners(row):
+    scores = row[2:].fillna(0)
+    print(type(scores))
     max_score = max(scores)
+    print(max_score)
     winners = [col for col in row.index if row[col] == max_score]
+    print("This is the winner :", winners)
     return "/".join(winners)
 
 # Application de la fonction sur chaque ligne du DataFrame pour créer la colonne "Victoire"
-df["Victoire"] = df.apply(get_winner, axis=1)
+stat_df["Victoire"] = stat_df.apply(get_winners, axis=1)
 
 # Affichage du titre
 st.title("Bilan du quizz de JiJi 2023")
 
 # Affichage du DataFrame
-st.write(df)
+st.write(stat_df)
